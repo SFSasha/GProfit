a3118e5bad5a56870a54fea2c837e255f7e23ae6
SFSasha <147089715+SFSasha@users.noreply.github.com>
2025-10-26 14:53:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-BOT_TOKEN = "8409940090:AAFib5pJwza6sDt1SrVU2ZTi2gHDtTLbh6I"
+BOT_TOKEN = "8419720606:AAELtdkAxDkfvJRpKL0FAypc0davQT4U_10"
 ADMIN_ID = [1500618394]
 
 REWARD_API_URL = "https://test.adsgram.ai/reward?userid=[userId]"
+
